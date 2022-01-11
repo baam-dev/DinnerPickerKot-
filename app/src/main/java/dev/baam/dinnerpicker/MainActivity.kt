bbea3ec4b2c0a1fce506ce09763c121d2855c4d0
Amir Bakhshi <amirmobili7@gmail.com>
2022-01-11 13:42:11 +0100

    layout setup

--- app/src/main/java/dev/baam/dinnerpicker/MainActivity.kt
+++ app/src/main/java/dev/baam/dinnerpicker/MainActivity.kt
@@ -2,10 +2,21 @@ package dev.baam.dinnerpicker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
+
+    // inheriting certain attributes from this class
 class MainActivity : AppCompatActivity() {
+    // viewDidLoad()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        selectedFoodText.text = "Burger King"
     }
+
+
+        
+
+
 }
